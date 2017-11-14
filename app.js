6e8fcd0a0a7a5f9643ecee6e92e8bd26543ab38c
danielcsulb <danielmendia86@gmail.com>
2017-11-13 16:14:49 -0800

    comit

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ const candidates = [
     { name: 'dash', likes: '0' },
 ]
 
-function list() {
-    for (var i = 0; i < candidates.length; i++) {
-        document.write((i + 1) + ": " + candidates[i] + "<br/>")
 
-    }
+var likes = 0;
+
+function like() {
+    document.getElementById("show").innerHTML = likes;
+    likes = likes + 1;
 }
+
+function candidateslist() {
+    var item = document.getElementById("candidateinput").value
+    var text = document.createTextNode(item)
+    var newitem = document.createElement("li")
+    newitem.appendChild(text)
+    document.getElementById("candidateslist").appendChild(newitem)
+}
+
+
+// function list() {
+// for (var i = 0; i < candidates.length; i++) {
+// document.write((i + 1) + ": " + candidates[i] + "<br/>")
+
+// }
+// }
 
 const output = document.getElementById("output");
 // output.innerHTML = 'The current messages are: ';
