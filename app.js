ae38b43156d039e8023a64fec347233b092ff3e3
danielcsulb <danielmendia86@gmail.com>
2017-11-17 22:19:56 -0800

    comit

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ function createCandidate() {
     cell2.innerHTML = '<p id ="likeNum"> 0 </p>';
     cell3.innerHTML = '<button style="font-size:24px" onclick="addLike(this)">Like <i class="fa fa-thumbs-o-up"></i></button>';
     cell4.innerHTML = '<button style="font-size:24px" onclick="deleteRow(this)">Delete <i class="fa fa-eraser"></i></button>';
-    document.getElementById('candidateForm').innerHTML = '<input type="text" id="candidateInput" placeholder="Name"><button style="font-size:24px" onclick="createCandidate()">Create<i class="fa fa-user-plus"></i></button>';
+    document.getElementById('candidateForm').innerHTML = '<input style="font-size:24px" type="text" id="candidateInput" placeholder="Name"><button style="font-size:24px" onclick="createCandidate()">Create<i class="fa fa-user-plus"></i></button>';
 
 }
