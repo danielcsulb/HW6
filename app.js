261f8eedf3815049d0106f43242b313d11de5dc6
danielcsulb <danielmendia86@gmail.com>
2017-11-18 00:09:43 -0800

    comit

--- app.js
+++ app.js
@@ -32,20 +32,20 @@ function createCandidate() {
         alert('Please enter a name');
         return;
 
+    } else {
+
+
+        var x = document.getElementById("candidateTable").rows.length;
+        var table = document.getElementById("candidateTable");
+        var row = table.insertRow(x);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        cell1.innerHTML = document.getElementById('candidateInput').value;
+        cell2.innerHTML = '<p id ="likeNum"> 0 </p>';
+        cell3.innerHTML = '<button style="font-size:20px" onclick="addLike(this)">Like <i class="fa fa-thumbs-o-up"></i></button>';
+        cell4.innerHTML = '<button style="font-size:20px" onclick="deleteRow(this)">Delete <i class="fa fa-eraser"></i></button>';
+        document.getElementById('candidateForm').innerHTML = '<h3>Create a Candidate</h3><input style="font-size:20px" type="text" id="candidateInput" placeholder="Name"><button style="font-size:20px" onclick="createCandidate()">Create<i class="fa fa-user-plus"></i></button>';
     }
-
-
-    var x = document.getElementById("candidateTable").rows.length;
-    var table = document.getElementById("candidateTable");
-    var row = table.insertRow(x);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    cell1.innerHTML = document.getElementById('candidateInput').value;
-    cell2.innerHTML = '<p id ="likeNum"> 0 </p>';
-    cell3.innerHTML = '<button style="font-size:20px" onclick="addLike(this)">Like <i class="fa fa-thumbs-o-up"></i></button>';
-    cell4.innerHTML = '<button style="font-size:20px" onclick="deleteRow(this)">Delete <i class="fa fa-eraser"></i></button>';
-    document.getElementById('candidateForm').innerHTML = '<h3>Create a Candidate</h3><input style="font-size:20px" type="text" id="candidateInput" placeholder="Name"><button style="font-size:20px" onclick="createCandidate()">Create<i class="fa fa-user-plus"></i></button>';
-
 }
