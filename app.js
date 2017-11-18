7f0748f10aff709178afeeadbef5494d4640858a
danielcsulb <danielmendia86@gmail.com>
2017-11-17 21:46:31 -0800

    comit

--- app.js
+++ app.js
@@ -1,44 +1,51 @@
-const candidates = [
-    { name: 'Peter Parker', likes: '700' },
-    { name: 'Tony Stark', likes: '135' },
-    { name: 'dash', likes: '0' },
-]
+// var CandidateList = [
+//     { name: 'Peter Parker', likes: '700' },
+//     { name: 'Tony Stark', likes: '135' },
+//     { name: 'Dash', likes: '0' },
+// ];
+
+// var printList = function() {
+//     candidateList.innerHTML = name;
+// }
 
 
-var likes = 1;
 
-function like() {
-    document.getElementById("show").innerHTML = likes;
-    likes = likes + 1;
+function addLike(r) {
+    var a = r.parentNode.parentNode.cells[1].textContent;
+    var b = parseInt(a) + 1;
+    r.parentNode.parentNode.cells[1].innerHTML = b;
+
 }
 
-function candidatesList() {
 
-    var item = document.getElementById('candidateInput').value
-
-    var text = document.createTextNode(item)
-
-    var newItem = document.createElement('li')
-
-    newItem.appendChild(text)
-
-    candidates.push(newItem);
-
-    output.innerHTML = list()
-
-    // document.getElementById('candidatesList').appendChild(newItem)
+function deleteRow(r) {
+    var c = r.parentNode.parentNode.rowIndex;
+    document.getElementById("candidateTable").deleteRow(c);
 }
 
-function list() {
-    // candidateInput.push(candidates);
-    for (var i = 0; i < candidates.length; i++) {
-        document.getElementById("output").innerHTML = (i + 1) + ": " + candidates[i] + "<br/>"
+
+
+
+function createCandidate() {
+
+    if (document.getElementById('candidateInput').value == '') {
+        alert('Please enter a name');
+        return;
 
     }
-}
 
-//         const output = document.getElementById("output");
-//         output.innerHTML = 'The current messages are: ';
-//         output2.innerHTML = list().innerHTML;
-//     }
-// }
+
+    var x = document.getElementById("candidateTable").rows.length;
+    var table = document.getElementById("candidateTable");
+    var row = table.insertRow(x);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = document.getElementById('candidateInput').value;
+    cell2.innerHTML = '<p id ="likeNum"> 0 </p>';
+    cell3.innerHTML = '<input type = "button" value ="Like" onclick = "addLike(this)" />';
+    cell4.innerHTML = '<input type = "button" value ="Delete" onclick = "deleteRow(this)" />';
+    document.getElementById('candidateForm').innerHTML = '<input type="text" id="candidateInput" placeholder="Name"><button type="button" onclick="createCandidate()">Create</button>'
+
+}
